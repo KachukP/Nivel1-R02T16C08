477cb3f5ad5df63b3654bc54a4b6514425dd7671
Pablo Kachuk <kachuck_p665@hotmail.com>
2025-04-29 15:24:01 -0300

    Consignas realizadas

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,33 @@
 import products from "./products.json";
 
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-  id: number;
-  name: string;
-  price: number;
-}
+  constructor(name:string,price:number) {
+    this.name=name;
+    this.price=price;
+  };
+  id:number;
+  name:string;
+  price:number;
+  static findProductsBelow(param1:number){
+    const list=products.filter((P)=>{
+        return (P.price<param1);
+    });
+    return list;
+  };
+};
 
 class User {
-  constructor(name: string) {
-    this.name = name;
-  }
-  name: string;
-  products: Product[] = [];
-  addProduct(newProduct: Product) {
+  constructor(name:string) {
+    this.name=name;
+  };
+  name:string;
+  products:Product[]=[];
+  addProduct(newProduct:Product) {
     this.products.push(newProduct);
-  }
-  addProducts(newProducts: Product[]) {
-    // esto no funciona:
-    this.products.push(newProducts);
-    // pista: push no suma muchos items (agrega de a uno)
-  }
-}
+  };
+  addProducts(paraNuePro2:Product[]) {
+    this.products=this.products.concat(paraNuePro2);
+  };
+};
 
-export { User, Product };
+export {User,Product};
